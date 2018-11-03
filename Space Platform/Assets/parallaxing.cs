b9ad474c48b993691a6dde54d459f3c6c34cd3f6
Djouuuu <44727134+Djouuuu@users.noreply.github.com>
2018-11-03 19:42:22 +0100

    para

---
+++ Space Platform/Assets/parallaxing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallaxing : MonoBehaviour {
+
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing; = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
