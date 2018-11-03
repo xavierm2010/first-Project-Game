53e2cb97eb39eb21169766beda0b7290c546c513
xavierm2010 <44727173+xavierm2010@users.noreply.github.com>
2018-11-03 17:47:41 +0100

    a

--- Space Platform/Assets/mouvement.cs
+++ Space Platform/Assets/mouvement.cs
@@ -6,15 +6,22 @@ public class mouvement : MonoBehaviour {
 
     public float runSpeed = 40f;
     float HorizontalMove = 0f;
+    bool jump;
 
 	void Update ()
     {
         HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            jump = true;
+        }
 	}
 
     void FixedUpdate()
     {
-        control.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
+        control.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 
 }
