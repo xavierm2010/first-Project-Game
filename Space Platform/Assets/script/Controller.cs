8b97be2c7e96af437cbc35837b8133751da6400e
xavierm2010 <44727173+xavierm2010@users.noreply.github.com>
2018-11-04 23:34:47 +0100

    double saut

--- Space Platform/Assets/script/Controller.cs
+++ Space Platform/Assets/script/Controller.cs
@@ -36,6 +36,7 @@ public class Controller : MonoBehaviour
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
     private Vector3 m_Velocity = Vector3.zero;
+    int JumpCont = 0;
 
 
 
@@ -84,15 +85,13 @@ public class Controller : MonoBehaviour
 
 
     private void FixedUpdate()
-
     {
-
         bool wasGrounded = m_Grounded;
-
-        m_Grounded = false;
-
-
-
+        if(JumpCont > 1)
+        {
+            m_Grounded = false;
+        }
+ 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 
         // This can be done using layers instead but Sample Assets will not overwrite your project settings.
@@ -100,23 +99,18 @@ public class Controller : MonoBehaviour
         Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
 
         for (int i = 0; i < colliders.Length; i++)
-
         {
 
             if (colliders[i].gameObject != gameObject)
-
             {
-
                 m_Grounded = true;
-
+                JumpCont = 0;
                 if (!wasGrounded)
-
+                {
                     OnLandEvent.Invoke();
-
+                }
             }
-
         }
-
     }
 
 
@@ -124,8 +118,12 @@ public class Controller : MonoBehaviour
 
 
     public void Move(float move, bool crouch, bool jump)
-
     {
+        if(jump == true)
+        {
+            JumpCont++;
+        }
+        Debug.Log(m_Grounded);
 
         // If crouching, check to see if the character can stand up
 
@@ -251,17 +249,15 @@ public class Controller : MonoBehaviour
         // If the player should jump...
 
         if (m_Grounded && jump)
-
         {
-
             // Add a vertical force to the player.
-
-            m_Grounded = false;
+            if(JumpCont > 1)
+            {
+                m_Grounded = false;
+            }
 
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-
         }
-
     }
 
 
