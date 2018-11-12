c55fc335a81cf269ff133fb77dbb6b3626d2570c
xavierm2010 <44727173+xavierm2010@users.noreply.github.com>
2018-11-12 09:57:26 +0100

    player health bar

---
+++ Space Platform/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public PlayerHealthBar Health;
+    public float PVMax = 100;
+    private float PV;
+
+	void Start ()
+    {
+        PV = PVMax;
+	}
+	
+	
+	void Update ()
+    {
+        PV = PV - (1 * Time.deltaTime);
+        //Health.fill(PV, PVMax);
+	}
+}
---
+++ Space Platform/Assets/script/PlayerHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour {
+
+    private Image HealthBar;
+    public float PVMax = 100;
+    private float PV;
+
+    void Start ()
+    {
+        HealthBar = GetComponent<Image>();
+        PV = PVMax;
+	}
+	
+	void Update()
+    {
+        PV = PV - (5 * Time.deltaTime);
+        HealthBar.fillAmount = PV / PVMax;
+    }
+}
