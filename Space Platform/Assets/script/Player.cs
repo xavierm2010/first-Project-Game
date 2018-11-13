9012834bb035497c1d99103a64d733ab58ba0012
xavierm2010 <44727173+xavierm2010@users.noreply.github.com>
2018-11-13 22:22:27 +0100

    des trucs

--- Space Platform/Assets/script/Player.cs
+++ Space Platform/Assets/script/Player.cs
@@ -2,26 +2,139 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public float speed = 300f;
+    public float jumpforce = 700f;
+    public int ExtraJumpsValue = 0;
+    public float DashForce = 300f;
+    public float PVMax = 100;
+    public float PushBackX = 1000f;
+    public float PushBackY = 1000f;
+    private float MoveInput;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    private bool isGrounded;            //ground check variable
+    public Transform groundCheck;
+    private float checkRadius = 0.1f;
+    public LayerMask whatIsGround;
+
+    private int ExtraJumps;
+    private bool jump = false;
+    public Animator anim;
+    private bool isMoving;
+
+    bool dash = false;
+    private float DashSpeed;
 
     public PlayerHealthBar Health;
 
-    public float PVMax = 100;
     private float PV;
+    private bool PushBack = false;
 
-	void Start ()
+
+    void Start()
     {
+        ExtraJumps = ExtraJumpsValue - 1;
+        rb = GetComponent<Rigidbody2D>();
         PV = PVMax;
-	}
-	
-	
-	void Update ()
-    {
-        Health.fill(PV, PVMax);
-	}
+    }
 
+    void FixedUpdate()
+    {
+        //verifie si il est sur le sol
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        rb.velocity = new Vector2(MoveInput * speed * Time.deltaTime, rb.velocity.y);
+
+        if (jump == true)
+        {
+            rb.velocity = Vector2.up * jumpforce * Time.deltaTime;
+            jump = false;
+        }
+
+        if (dash == true)
+        {
+            rb.velocity = new Vector2(DashSpeed, rb.velocity.y);
+            dash = false;
+        }
+
+        if ((PushBack == true) && (facingRight == true))
+        {
+            rb.velocity = new Vector2(-PushBackX, PushBackY);
+            PushBack = false;
+        }
+        else if ((PushBack == true) && (facingRight == false))
+        {
+            rb.velocity = new Vector2(PushBackX, PushBackY);
+            PushBack = false;
+        }
+    }
+
+    void Update()
+    {
+        //remplissage de la bar de vie
+        Health.fill(PV, PVMax);
+
+
+        //deplacement
+        MoveInput = Input.GetAxisRaw("Horizontal");
+        if (facingRight == false && MoveInput > 0)
+        {
+            flip();
+        }
+        else if (facingRight == true && MoveInput < 0)
+        {
+            flip();
+        }
+
+
+        //Compteur de saut
+        if (isGrounded == true)
+        {
+            ExtraJumps = ExtraJumpsValue - 1;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && ExtraJumps >= 0)
+        {
+            ExtraJumps--;
+            jump = true;
+        }
+
+
+        //dash
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            DashSpeed = DashForce * Time.deltaTime * MoveInput;
+            dash = true;
+        }
+
+
+        //animator
+        if (MoveInput > 0 || MoveInput < 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        anim.SetBool("moving", isMoving);
+    }
+
+
+    //retourne le sprite du perso de 180 degrer
+    void flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+
+    //execution des dega
     public void gotHit(float damage)
     {
         PV -= damage;
+        PushBack = true;
     }
 }
